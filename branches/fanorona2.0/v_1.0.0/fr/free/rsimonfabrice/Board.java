00031a2c76c8725ae612d7d6ac72881b48c8e4f6
rabarijaona@gmail.com <rabarijaona@gmail.com@c5872d50-bd30-0410-8862-5d2c5c102a8f>
2012-10-16 21:11:21 +0000

    initialisation d'une nouvelle branche 2.0

---
+++ branches/fanorona2.0/v_1.0.0/fr/free/rsimonfabrice/Board.java
@@ -0,0 +1,27 @@
+package fr.free.rsimonfabrice;
+
+import java.util.Collection;
+/**
+ * 
+ * @author fabricerabarijaona
+ *$Rev$:     Revision of last commit
+ *$Author$:  Author of last commit
+ *$Date$:    Date of last commit
+ */
+public class Board {
+	Collection<Case> listCase;
+	Collection<Piece> listPiece;
+	public Collection<Case> getListCase() {
+		return listCase;
+	}
+	public void setListCase(Collection<Case> listCase) {
+		this.listCase = listCase;
+	}
+	public Collection<Piece> getListPiece() {
+		return listPiece;
+	}
+	public void setListPiece(Collection<Piece> listPiece) {
+		this.listPiece = listPiece;
+		Le trunk avance 
+	}
+}
